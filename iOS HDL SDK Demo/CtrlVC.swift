890449fc1139cce4378e9cec163b0fe69679dd75
TommyDai <hdl-djl@hdlchina.com.cn>
2018-05-09 13:05:52 +0800

    Update v0.1.6.Modify the control timeout logic.

--- iOS HDL SDK Demo/CtrlVC.swift
+++ iOS HDL SDK Demo/CtrlVC.swift
@@ -479,6 +479,7 @@ class CtrlVC: UIViewController,LightCtrlDelegate,CurtainCtrlDelegate,ACCtrlDeleg
     }
     
     func getSceneCtrlInfo(sceneCtrlBackInfo: SceneCtrlBackInfo) {
+        
         if(sceneCtrlBackInfo.appliancesInfo?.deviceSubnetID == info!.deviceSubnetID
             && sceneCtrlBackInfo.appliancesInfo?.deviceDeviceID == info!.deviceDeviceID
             && sceneCtrlBackInfo.areaNum == info!.logicMode!.areaNum
